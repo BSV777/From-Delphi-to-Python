2581346c8b01e2a7763125b7cd536789f89d55fa
BSV777 <baykov-sv@yandex.ru>
2017-04-20 16:15:49 +0300

    Главная форма пересоздана в дизайнере

---
+++ Python-project/BaseWindow.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'BaseWindow.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+
+
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+
+class Ui_BaseWindow(object):
+    def setupUi(self, BaseWindow):
+        BaseWindow.setObjectName(_fromUtf8("BaseWindow"))
+        BaseWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(BaseWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(30, 30, 341, 441))
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        BaseWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(BaseWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 27))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.m_file = QtGui.QMenu(self.menubar)
+        self.m_file.setObjectName(_fromUtf8("m_file"))
+        self.m_object = QtGui.QMenu(self.menubar)
+        self.m_object.setObjectName(_fromUtf8("m_object"))
+        self.m_help = QtGui.QMenu(self.menubar)
+        self.m_help.setObjectName(_fromUtf8("m_help"))
+        BaseWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(BaseWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        BaseWindow.setStatusBar(self.statusbar)
+        self.a_create = QtGui.QAction(BaseWindow)
+        self.a_create.setObjectName(_fromUtf8("a_create"))
+        self.a_open = QtGui.QAction(BaseWindow)
+        self.a_open.setObjectName(_fromUtf8("a_open"))
+        self.a_save = QtGui.QAction(BaseWindow)
+        self.a_save.setObjectName(_fromUtf8("a_save"))
+        self.a_saveas = QtGui.QAction(BaseWindow)
+        self.a_saveas.setObjectName(_fromUtf8("a_saveas"))
+        self.a_saveasweb = QtGui.QAction(BaseWindow)
+        self.a_saveasweb.setObjectName(_fromUtf8("a_saveasweb"))
+        self.a_openasweb = QtGui.QAction(BaseWindow)
+        self.a_openasweb.setObjectName(_fromUtf8("a_openasweb"))
+        self.a_exit = QtGui.QAction(BaseWindow)
+        self.a_exit.setObjectName(_fromUtf8("a_exit"))
+        self.a_createbt = QtGui.QAction(BaseWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("icons/bt.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.a_createbt.setIcon(icon)
+        self.a_createbt.setObjectName(_fromUtf8("a_createbt"))
+        self.a_createed = QtGui.QAction(BaseWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8("icons/ed.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.a_createed.setIcon(icon1)
+        self.a_createed.setObjectName(_fromUtf8("a_createed"))
+        self.a_createlb = QtGui.QAction(BaseWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(_fromUtf8("icons/lb.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.a_createlb.setIcon(icon2)
+        self.a_createlb.setObjectName(_fromUtf8("a_createlb"))
+        self.a_delobj = QtGui.QAction(BaseWindow)
+        self.a_delobj.setObjectName(_fromUtf8("a_delobj"))
+        self.a_about = QtGui.QAction(BaseWindow)
+        self.a_about.setObjectName(_fromUtf8("a_about"))
+        self.m_file.addAction(self.a_create)
+        self.m_file.addAction(self.a_open)
+        self.m_file.addSeparator()
+        self.m_file.addAction(self.a_save)
+        self.m_file.addAction(self.a_saveas)
+        self.m_file.addAction(self.a_saveasweb)
+        self.m_file.addSeparator()
+        self.m_file.addAction(self.a_openasweb)
+        self.m_file.addSeparator()
+        self.m_file.addAction(self.a_exit)
+        self.m_object.addAction(self.a_createbt)
+        self.m_object.addAction(self.a_createed)
+        self.m_object.addAction(self.a_createlb)
+        self.m_object.addSeparator()
+        self.m_object.addAction(self.a_delobj)
+        self.m_help.addAction(self.a_about)
+        self.menubar.addAction(self.m_file.menuAction())
+        self.menubar.addAction(self.m_object.menuAction())
+        self.menubar.addAction(self.m_help.menuAction())
+
+        self.retranslateUi(BaseWindow)
+        QtCore.QMetaObject.connectSlotsByName(BaseWindow)
+
+    def retranslateUi(self, BaseWindow):
+        BaseWindow.setWindowTitle(_translate("BaseWindow", "MainWindow", None))
+        self.label.setText(_translate("BaseWindow", "TextLabel", None))
+        self.m_file.setTitle(_translate("BaseWindow", "&Файл", None))
+        self.m_object.setTitle(_translate("BaseWindow", "&Объект", None))
+        self.m_help.setTitle(_translate("BaseWindow", "&Справка", None))
+        self.a_create.setText(_translate("BaseWindow", "Созд&ать", None))
+        self.a_create.setStatusTip(_translate("BaseWindow", "Создать схему размещения", None))
+        self.a_create.setShortcut(_translate("BaseWindow", "Ctrl+N", None))
+        self.a_open.setText(_translate("BaseWindow", "&Открыть", None))
+        self.a_open.setStatusTip(_translate("BaseWindow", "Открыть схему размещения объектов", None))
+        self.a_open.setShortcut(_translate("BaseWindow", "Ctrl+O", None))
+        self.a_save.setText(_translate("BaseWindow", "&Сохранить", None))
+        self.a_save.setStatusTip(_translate("BaseWindow", "Сохранить схему размещения объектов", None))
+        self.a_save.setShortcut(_translate("BaseWindow", "Ctrl+S", None))
+        self.a_saveas.setText(_translate("BaseWindow", "Сохранить &как", None))
+        self.a_saveas.setStatusTip(_translate("BaseWindow", "Сохранить схему размещения объектов как...", None))
+        self.a_saveasweb.setText(_translate("BaseWindow", "Сохранить как Web-страни&цу", None))
+        self.a_saveasweb.setStatusTip(
+            _translate("BaseWindow", "Сохранить схему размещения объектов как Web-страницу", None))
+        self.a_openasweb.setText(_translate("BaseWindow", "Открыть как &Web-страницу", None))
+        self.a_openasweb.setStatusTip(
+            _translate("BaseWindow", "Открыть схему размещения объектов как Web-страницу", None))
+        self.a_exit.setText(_translate("BaseWindow", "В&ыход", None))
+        self.a_exit.setStatusTip(_translate("BaseWindow", "Выход из программы", None))
+        self.a_exit.setShortcut(_translate("BaseWindow", "Ctrl+Q", None))
+        self.a_createbt.setText(_translate("BaseWindow", "Создать &кнопку", None))
+        self.a_createbt.setStatusTip(_translate("BaseWindow", "Создать кнопку на форме", None))
+        self.a_createed.setText(_translate("BaseWindow", "Создать &поле ввода", None))
+        self.a_createed.setStatusTip(_translate("BaseWindow", "Создать поле ввода на форме", None))
+        self.a_createlb.setText(_translate("BaseWindow", "Создать &надпись", None))
+        self.a_createlb.setStatusTip(_translate("BaseWindow", "Создать надпись на форме", None))
+        self.a_delobj.setText(_translate("BaseWindow", "У&далить объект", None))
+        self.a_delobj.setStatusTip(_translate("BaseWindow", "Удалить выбранный объект", None))
+        self.a_delobj.setShortcut(_translate("BaseWindow", "Del", None))
+        self.a_about.setText(_translate("BaseWindow", "&О программе", None))
+        self.a_about.setStatusTip(_translate("BaseWindow", "Сведения о программе", None))
+        self.a_about.setShortcut(_translate("BaseWindow", "F1", None))
--- Python-project/MainWindow.py
+++ Python-project/MainWindow.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtGui
 
 from PyQt4.QtGui import *
 
+import BaseWindow  # Импортируем базовую форму, нарисованную в дизайнере
 
-class MainWindow(QtGui.QMainWindow):  # Объявляем класс MainWindow, наследующий свойства класса QMainWindow модуля QtGui
+
+# Класс MainWindow, унаследованный от QMainWindow и Ui_BaseWindow - из дизайнера
+class MainWindow(QMainWindow, BaseWindow.Ui_BaseWindow):
 
     # Внутренние процедуры класса
     # ------------------------------------------------------------------------------
-
     # TODO: Работать здесь!
     # ------------------------------------------------------------------------------
 
     def _select(self):
         print("_select")  # DEBUG: Отладочный вывод
-        print("test1")
+
         # scrollArea = QtGui.QScrollArea(self)
-        label2 = QtGui.QLabel(u"Надпись2", self)  # Создали объект надпись класса QLabel
-        # self.setWidget(label2)
-        label2.setParent = self
-        label2.move(80, 80)
-        print("test2")
+        # self.label2 = QtGui.QLabel(u"Надпись2", self)  # Создали объект надпись класса QLabel
+        # self.label2.move(80, 80)
+
+        # self.items_ly.addWidget(label2)
+        # self.add
+        # addWidget(label2)
+        # self.label2.setParent =
+
+
         # scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         # scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         # scrollArea.setParent(self)
@@ -38,14 +44,18 @@ class MainWindow(QtGui.QMainWindow):  # Объявляем класс MainWindow
 
     def _createbt(self):
         print("_createbt")  # DEBUG: Отладочный вывод
-        self.label2 = QtGui.QLabel(u"88888888")  # Создали объект надпись класса QLabel
-        self.label2.move(10, 90)
+        self.verticalLayout.addWidget(QtGui.QPushButton(u"Кнопка", self))
+
 
     def _createed(self):
         print("_createed")  # DEBUG: Отладочный вывод
+        self.verticalLayout.addWidget(QtGui.QLineEdit(u"Поле", self))
+
 
     def _createlb(self):
         print("_createlb")  # DEBUG: Отладочный вывод
+        self.verticalLayout.addWidget(QtGui.QLabel(u"Надпись", self))
+
 
     def _delobj(self):
         print("_delobj")  # DEBUG: Отладочный вывод
@@ -78,14 +88,14 @@ class MainWindow(QtGui.QMainWindow):  # Объявляем класс MainWindow
 
     def __init__(self, parent=None):  # Объявляем конструктор класса MyWindow
         QtGui.QWidget.__init__(self,
-                               parent)  # Сначала выполняем действия, предусмотренные конструктором родительского класса (QWidget)
+                               parent)  # Сначала выполняем действия, предусмотренные конструктором родительского класса
+        self.setupUi(self)  # Вызов метода, унаследованного от Ui_BaseWindow для инициализации виджетов класса
 
         self.setFont(QFont('SansSerif', 9))  # Устанавливаем шрифт
 
+
         # TODO: 1. Добавить скроллбары
         # ------------------------------------------------------------------------------
-
-
         # scrollArea.setWidget(label2)
         # self.setWidget(self.scrollArea)
 
@@ -108,101 +118,102 @@ class MainWindow(QtGui.QMainWindow):  # Объявляем класс MainWindow
 
         # TEST: Тестовые объекты
         # ------------------------------------------------------------------------------
-        label1 = QtGui.QLabel(u"Надпись", self)  # Создали объект надпись класса QLabel
-        label1.move(120, 200)
-
-        edit1 = QtGui.QLineEdit(u"Поле", self)  # Создали объект поле ввода
-        edit1.move(150, 160)
-
-        button1 = QtGui.QPushButton(u"Кнопка", self)  # Создали объект кнопка, экземпляр класса QPushButton
-        button1.move(200, 200)
-
-        # ------------------------------------------------------------------------------
+        # label1 = QtGui.QLabel(u"Надпись", self)         # Создали объект надпись класса QLabel
+        # label1.move(120, 200)
+        #
+        # edit1 = QtGui.QLineEdit(u"Поле", self)          # Создали объект поле ввода
+        # edit1.move(150, 160)
+        #
+        # button1 = QtGui.QPushButton(u"Кнопка", self)    # Создали объект кнопка, экземпляр класса QPushButton
+        # button1.move(200, 200)
 
 
-        # Создание меню
-        # ------------------------------------------------------------------------------
-        menubar = self.menuBar()  # Создание меню
+        # # ==============================================================================
+        # # Создание меню - код рабочий, но не используется - перенесено в дизайнер
+        # # ------------------------------------------------------------------------------
+        # menubar = self.menuBar()  # Создание меню
+        #
+        # # Меню Файл
+        # a_create = QtGui.QAction(u"Созд&ать", self)
+        # a_create.setShortcut('Ctrl+N')
+        # a_create.setStatusTip(u"Создать схему размещения")
+        #
+        # a_open = QtGui.QAction(u"&Открыть", self)
+        # a_open.setShortcut('Ctrl+O')
+        # a_open.setStatusTip(u"Открыть схему размещения объектов")
+        #
+        # a_save = QtGui.QAction(u"&Сохранить", self)
+        # a_save.setShortcut('Ctrl+S')
+        # a_save.setStatusTip(u"Сохранить схему размещения объектов")
+        #
+        # a_saveas = QtGui.QAction(u"Сохранить &как", self)
+        # a_saveas.setStatusTip(u"Сохранить схему размещения объектов как...")
+        #
+        # a_saveasweb = QtGui.QAction(u"Сохранить как Web-страни&цу", self)
+        # a_saveasweb.setStatusTip(u"Сохранить схему размещения объектов как Web-страницу")
+        #
+        # a_openasweb = QtGui.QAction(u"Открыть как &Web-страницу", self)
+        # a_openasweb.setStatusTip(u"Открыть схему размещения объектов как Web-страницу")
+        #
+        # a_exit = QtGui.QAction(u"В&ыход", self)
+        # a_exit.setShortcut('Ctrl+Q')
+        # a_exit.setStatusTip(u"Выход из программы")
+        #
+        # m_file = menubar.addMenu(u"&Файл")
+        # m_file.addAction(a_create)              # Строка меню
+        # m_file.addAction(a_open)                # Строка меню
+        # m_file.addSeparator()                   # Разделитель меню
+        # m_file.addAction(a_save)                # Строка меню
+        # m_file.addAction(a_saveas)              # Строка меню
+        # m_file.addAction(a_saveasweb)           # Строка меню
+        # m_file.addSeparator()                   # Разделитель меню
+        # m_file.addAction(a_openasweb)           # Строка меню
+        # m_file.addSeparator()                   # Разделитель меню
+        # m_file.addAction(a_exit)                # Строка меню
+        #
+        # # Меню Объект
+        # a_createbt = QtGui.QAction(QtGui.QIcon('icons/bt.png'), u"Создать &кнопку", self)
+        # a_createbt.setStatusTip(u"Создать кнопку на форме")
+        #
+        # a_createed = QtGui.QAction(QtGui.QIcon('icons/ed.png'), u"Создать &поле ввода", self)
+        # a_createed.setStatusTip(u"Создать поле ввода на форме")
+        #
+        # a_createlb = QtGui.QAction(QtGui.QIcon('icons/lb.png'), u"Создать &надпись", self)
+        # a_createlb.setStatusTip(u"Создать надпись на форме")
+        #
+        # a_delobj = QtGui.QAction(u"У&далить объект", self)
+        # a_delobj.setShortcut('DEL')
+        # a_delobj.setStatusTip(u"Удалить выбранный объект")
+        #
+        # m_object = menubar.addMenu(u"&Объект")
+        # m_object.addAction(a_createbt)          # Строка меню
+        # m_object.addAction(a_createed)          # Строка меню
+        # m_object.addAction(a_createlb)          # Строка меню
+        # m_object.addSeparator()                 # Разделитель меню
+        # m_object.addAction(a_delobj)            # Строка меню
+        #
+        # # Меню Справка
+        # a_about = QtGui.QAction(u"&О программе", self)
+        # a_about.setShortcut('F1')
+        # a_about.setStatusTip(u"Сведения о программе")
+        #
+        # m_help = menubar.addMenu(u"&Справка")
+        # m_help.addAction(a_about)  # Строка меню
+        # # ==============================================================================
 
-        # Подменю Файл
-        a_create = QtGui.QAction(u"Созд&ать", self)
-        a_create.setShortcut('Ctrl+N')
-        a_create.setStatusTip(u"Создать схему размещения")
-        self.connect(a_create, QtCore.SIGNAL('triggered()'), self._create)
-
-        a_open = QtGui.QAction(u"&Открыть", self)
-        a_open.setShortcut('Ctrl+O')
-        a_open.setStatusTip(u"Открыть схему размещения объектов")
-        self.connect(a_open, QtCore.SIGNAL('triggered()'), self._open)
-
-        a_save = QtGui.QAction(u"&Сохранить", self)
-        a_save.setShortcut('Ctrl+S')
-        a_save.setStatusTip(u"Сохранить схему размещения объектов")
-        self.connect(a_save, QtCore.SIGNAL('triggered()'), self._save)
-
-        a_saveas = QtGui.QAction(u"Сохранить &как", self)
-        a_saveas.setStatusTip(u"Сохранить схему размещения объектов как...")
-        self.connect(a_saveas, QtCore.SIGNAL('triggered()'), self._saveas)
-
-        a_saveasweb = QtGui.QAction(u"Сохранить как Web-страни&цу", self)
-        a_saveasweb.setStatusTip(u"Сохранить схему размещения объектов как Web-страницу")
-        self.connect(a_saveasweb, QtCore.SIGNAL('triggered()'), self._saveasweb)
-
-        a_openasweb = QtGui.QAction(u"Открыть как &Web-страницу", self)
-        a_openasweb.setStatusTip(u"Открыть схему размещения объектов как Web-страницу")
-        self.connect(a_openasweb, QtCore.SIGNAL('triggered()'), self._openasweb)
-
-        a_exit = QtGui.QAction(u"В&ыход", self)
-        a_exit.setShortcut('Ctrl+Q')
-        a_exit.setStatusTip(u"Выход из программы")
-        self.connect(a_exit, QtCore.SIGNAL('triggered()'), self._exit)
-
-        m_file = menubar.addMenu(u"&Файл")
-        m_file.addAction(a_create)  # Строка меню
-        m_file.addAction(a_open)  # Строка меню
-        m_file.addSeparator()  # Разделитель меню
-        m_file.addAction(a_save)  # Строка меню
-        m_file.addAction(a_saveas)  # Строка меню
-        m_file.addAction(a_saveasweb)  # Строка меню
-        m_file.addSeparator()  # Разделитель меню
-        m_file.addAction(a_openasweb)  # Строка меню
-        m_file.addSeparator()  # Разделитель меню
-        m_file.addAction(a_exit)  # Строка меню
-
-        # Подменю Объект
-        a_createbt = QtGui.QAction(QtGui.QIcon('icons/bt.png'), u"Создать &кнопку", self)
-        a_createbt.setStatusTip(u"Создать кнопку на форме")
-        self.connect(a_createbt, QtCore.SIGNAL('triggered()'), self._createbt)
-
-        a_createed = QtGui.QAction(QtGui.QIcon('icons/ed.png'), u"Создать &поле ввода", self)
-        a_createed.setStatusTip(u"Создать поле ввода на форме")
-        self.connect(a_createed, QtCore.SIGNAL('triggered()'), self._createed)
-
-        a_createlb = QtGui.QAction(QtGui.QIcon('icons/lb.png'), u"Создать &надпись", self)
-        a_createlb.setStatusTip(u"Создать надпись на форме")
-        self.connect(a_createlb, QtCore.SIGNAL('triggered()'), self._createlb)
-
-        a_delobj = QtGui.QAction(u"У&далить объект", self)
-        a_delobj.setShortcut('DEL')
-        a_delobj.setStatusTip(u"Удалить выбранный объект")
-        self.connect(a_delobj, QtCore.SIGNAL('triggered()'), self._delobj)
-
-        m_object = menubar.addMenu(u"&Объект")
-        m_object.addAction(a_createbt)  # Строка меню
-        m_object.addAction(a_createed)  # Строка меню
-        m_object.addAction(a_createlb)  # Строка меню
-        m_object.addSeparator()  # Разделитель меню
-        m_object.addAction(a_delobj)  # Строка меню
-
-        # Подменю Справка
-        a_about = QtGui.QAction(u"&О программе", self)
-        a_about.setShortcut('F1')
-        a_about.setStatusTip(u"Сведения о программе")
-        a_about.triggered.connect(self._about)
-
-
-        m_help = menubar.addMenu(u"&Справка")
-        m_help.addAction(a_about)  # Строка меню
+        # Меню - связь сигналы-слоты
+        self.a_create.triggered.connect(self._create)
+        self.a_open.triggered.connect(self._open)
+        self.a_save.triggered.connect(self._save)
+        self.a_saveas.triggered.connect(self._saveas)
+        self.a_saveasweb.triggered.connect(self._saveasweb)
+        self.a_openasweb.triggered.connect(self._openasweb)
+        self.a_exit.triggered.connect(self._exit)
+        self.a_createbt.triggered.connect(self._createbt)
+        self.a_createed.triggered.connect(self._createed)
+        self.a_createlb.triggered.connect(self._createlb)
+        self.a_delobj.triggered.connect(self._delobj)
+        self.a_about.triggered.connect(self._about)
 
         # Создание тулбара
         # ------------------------------------------------------------------------------
@@ -212,14 +223,20 @@ class MainWindow(QtGui.QMainWindow):  # Объявляем класс MainWindow
 
         toolbar = self.addToolBar(u"Панель инструментов")
         toolbar.addAction(a_select)  # Кнопка тулбара
-        toolbar.addAction(a_createbt)  # Кнопка тулбара
-        toolbar.addAction(a_createed)  # Кнопка тулбара
-        toolbar.addAction(a_createlb)  # Кнопка тулбара
+        toolbar.addAction(self.a_createbt)  # Кнопка тулбара
+        toolbar.addAction(self.a_createed)  # Кнопка тулбара
+        toolbar.addAction(self.a_createlb)  # Кнопка тулбара
 
         # ------------------------------------------------------------------------------
 
         self.statusBar().showMessage(u"Готов")  # Первоначальная надпись в строке статуса
 
 
+# Тестирование интерфейса
 if __name__ == "__main__":
-    print(u"------ test ------")
+    app = QApplication([])  # Создаем объект app (приложение) - экземпляр класса QApplication
+    test = MainWindow()  # Создаем объект MainForm - экземпляр класса MainWindow()
+    test.setWindowTitle(u"test")  # Заголовок окна
+    test.resize(450, 250)  # Установили размеры окна
+    test.show()  # Отображаем окно
+    app.exec_()  # Запускаем цикл обработки событий объекта app (приложение)
