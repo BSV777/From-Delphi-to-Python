44c21917708cbbec2a1f5047b5c5404e8e829fd9
BSV777 <baykov-sv@yandex.ru>
2017-04-24 18:01:38 +0300

    Парсинг частично отлажен _2

--- Python-project/mParserTextFile.py
+++ Python-project/mParserTextFile.py
@@ -9,11 +9,38 @@ MINWIDTH = 21
 DEFAULTHEIGHT = 21
 DEFAULTWIDTH = 70
 
-def checkValid(arr):  # Проверка объекта на конфликт с предыдущими. Но IMHO с ошибкой в условии!?
+
+def correctLine(line):  # Чистка строк по краям, восстановление структуры строки
+    # Поиск и удаление лишних пробелов.
+    tempLine = line.strip()  # Удаляем пробелы по краям
+    if tempLine != "":
+        tempLine = re.sub(r'\s+', ' ', tempLine)  # Удаление двойных пробелов используя регулярные выражения
+        tempLine = re.sub(r'< ', '<', tempLine)  # Удаление пробела после символа <
+
+        # Считаем, что строки без < допустимы. Добавляем < и парсим дальше
+        n = tempLine.find('<')  # Находим позицию символа <
+        if n == -1:  # Нет символа <
+            tempLine = '<' + tempLine
+        else:
+            tempLine = tempLine[n:]  # Удаляем возможный мусор перед <
+
+        # Считаем, что строки без > допустимы. Добавляем > и парсим дальше
+        n = tempLine.find('>')  # Находим позицию символа >
+        if n == -1:  # Нет символа >
+            tempLine = tempLine + '>'
+        else:
+            tempLine = tempLine[:n + 1]  # Берем строку до > включительно
+    return tempLine
+
+
+def checkValid(arr, log):  # Проверка объекта на конфликт с предыдущими. Но IMHO с ошибкой в условии!?
     if len(arr) <= 1:  # Если в списке всего 0 или 1 элемент - он точно без конфликтов
         return True
     Valid = True
-    i = len(arr) - 1
+    i = len(arr) - 2
+
+    log.write("TEST: " + str(i) + "     " + str(arr[i]) + "\n")  # DEBUG: Отладочный вывод
+
     for n in range(i):
         if (arr[i]["top"]  + arr[i]["height"] > arr[n]["top"])    and (
             arr[i]["top"]  < arr[n]["top"]    + arr[n]["height"]) and (
@@ -40,37 +67,10 @@ def ParserTextFile(fileName, arr):
         # TODO: Переделать анализ строки с использованием split
 
         for L in f:  # Проверить правильно ли так, или нужно через readlines
-            L = L.strip()  # Удаляем пробелы по краям
-            if L == '':  # Если строка пустая - пропускаем
+            lineWork = correctLine(L)  # Чистка строк по краям, восстановление структуры строки
+            if lineWork == '':  # Если строка пустая - пропускаем
                 continue
 
-            logFile.write("\n")  # DEBUG: Отладочный вывод
-            logFile.write("LINE: " + L + "\n")  # DEBUG: Отладочный вывод
-
-            # Чистка строк по краям, восстановление структуры строки
-            # ----------------------------------------------------------------------------------------------------
-            lineWork = L  # Будем строку lineWork модифицировать. Исходная строка L нужна для логирования.
-
-            # Считаем, что строки без < допустимы. Добавляем < и парсим дальше.
-            n = lineWork.find('<')  # Находим позицию символа <
-            if n == -1:  # Нет символа <
-                lineWork = '<' + lineWork
-            else:
-                lineWork = lineWork[n:]  # Удаляем возможный мусор перед <
-
-            # Считаем, что строки без > допустимы. Добавляем > и парсим дальше.
-            n = lineWork.find('>')  # Находим позицию символа >
-            if n == -1:  # Нет символа >
-                lineWork = lineWork + '>'
-            else:
-                lineWork = lineWork[:n + 1]  # lineWork = Copy(lineWork, 1, n) Берем строку до > включительно
-
-            # Поиск и удаление лишних пробелов.
-            lineWork = re.sub(r'\s+', ' ', lineWork)  # Удаление двойных пробелов используя регулярные выражения
-            lineWork = re.sub(r'< ', '<', lineWork)   # Удаление пробела после символа <
-
-            #logFile.write("Clean line " + lineWork + "\n")  # DEBUG: Отладочный вывод
-            # ----------------------------------------------------------------------------------------------------
             # Создаем новый элемент списка
             arr.append({"text": "", "typ": 0, "left": 0, "top": 0, "width": 0, "height": 0, "colspan": 0, "rowspan": 0})
 
@@ -84,7 +84,7 @@ def ParserTextFile(fileName, arr):
                 arr[i]["typ"] = typ["Label"]
             else:  # Строка не содержит тип объекта - пропускаем
                 arr.pop()  # Удаляем последний пустой элемент списка
-                logFile.write("ERROR: " + L + " = wrong object type" + "\n")
+                logFile.write("ERROR: " + lineWork + " = wrong object type" + "\n")
                 Errs = True
                 continue
             # ----------------------------------------------------------------------------
@@ -96,87 +96,54 @@ def ParserTextFile(fileName, arr):
 
             # Заполнение свойств объекта
             # -----------------------------------------------------------------------------
-            n = lineWork.lower().find('left="')
+            n = lineWork.lower().find('left="')  # Заполнение свойства left, если указано
             if n != -1:
-            #     L1 = lineWork
-            #     Delete(L1, 1, Pos('left="', LowerCase(L1)) + 5)
-            #     L1 = Copy(L1, 1, Pos('"', L1) - 1)
+                L1 = lineWork[lineWork.lower().find('left="') + 6:]
+                L1 = L1[:L1.find('"')]
+                try:
+                    arr[i]["left"] = int(L1)
+                except ValueError:
+                    arr[i]["left"] = 0
 
-            L1.write(lineWork + "\n")
-
-            #     try:
-            #         arr[i]["left"] = StrToInt(L1)
-            #     except on E: EConvertError do
-            #         arr[i]["left"] = 0
-            #
-            n = lineWork.lower().find('top="')
+            n = lineWork.lower().find('top="')  # Заполнение свойства top, если указано
             if n != -1:
-            #     L1 = lineWork
-            #     Delete(L1, 1, Pos('top="', LowerCase(L1)) + 4)
-            #     L1 = Copy(L1, 1, Pos('"', L1) - 1)
+                L1 = lineWork[lineWork.lower().find('top="') + 5:]
+                L1 = L1[:L1.find('"')]
+                try:
+                    arr[i]["top"] = int(L1)
+                except ValueError:
+                    arr[i]["top"] = 0
 
-            L1.write(lineWork + "\n")
-
-            #     try:
-            #         arr[i]["top"] = StrToInt(L1)
-            #     except on E: EConvertError do
-            #         arr[i]["top"] = 0
-            #
-            #
-            n = lineWork.lower().find('width="')
+            n = lineWork.lower().find('width="')  # Заполнение свойства width, если указано
             if n != -1:
-            #     L1 = lineWork
-            #     Delete(L1, 1, Pos('width="', LowerCase(L1)) + 6)
-            #     L1 = Copy(L1, 1, Pos('"', L1) - 1)
+                L1 = lineWork[lineWork.lower().find('width="') + 7:]
+                L1 = L1[:L1.find('"')]
+                try:
+                    arr[i]["width"] = int(L1)
+                except ValueError:
+                    arr[i]["width"] = 0
 
-            L1.write(lineWork + "\n")
-
-            #     try:
-            #         arr[i]["width"] = StrToInt(L1)
-            #     except on E: EConvertError do
-            #         arr[i]["width"] = 0
-            #
-            # if arr[i]["typ"] != typ["TextEdit"]:
-            #     n = Pos('height="', LowerCase(lineWork))
-            #     if n != -1:
-            #         L1 = lineWork
-            #         Delete(L1, 1, Pos('height="', LowerCase(L1)) + 7)
-            #         L1 = Copy(L1, 1, Pos('"', L1) - 1)
-
-            L1.write(lineWork + "\n")
-
-            #         try:
-            #             arr[i]["height"] = StrToInt(L1)
-            #         except on E: EConvertError do
-            #             arr[i]["height"] = 0
-            #
-            n = lineWork.lower().find('caption="')
-            if n != -1:
-            #     L1 = L
-            #     Delete(L1, 1, Pos('caption', LowerCase(L1)) + 6)
-            #     Delete(L1, 1, Pos('"', L1))
-            #     L1 = Copy(L1, 1, Pos('"', L1) - 1)
-
-                L1.write(lineWork + "\n")
-
-                arr[i]["text"] = L1
-            #
-            n = lineWork.lower().find('text="')
-            if n != -1:
-                L1 = L
-
-                L1.write(lineWork + "\n")
-
-                n = L1.lower().find('textedit')
-
+            if arr[i]["typ"] != typ["TextEdit"]:  # Заполнение свойства height, если указано и если это не TextEdit
+                n = lineWork.lower().find('height="')
                 if n != -1:
-                    L1 = L1[:n + 7]  # Delete(L1, 1, n + 7)
+                    L1 = lineWork[lineWork.lower().find('height="') + 8:]
+                    L1 = L1[:L1.find('"')]
+                    try:
+                        arr[i]["height"] = int(L1)
+                    except ValueError:
+                        arr[i]["height"] = 0
 
-                L1 = L1[:L1.lower().find('text') + 3]  # Delete(L1, 1, Pos('text', LowerCase(L1)) + 3)
-                L1 = L1[:L1.lower().find('"')]  # Delete(L1, 1, Pos('"', L1))
-                L1 = L1[:L1.find('"') - 1]  # L1 = Copy(L1, 1, Pos('"', L1) - 1)
+            n = lineWork.lower().find('caption="')  # Заполнение свойства caption, если указано
+            if n != -1:
+                L1 = lineWork[lineWork.lower().find('caption="') + 9:]
+                L1 = L1[:L1.find('"')]
                 arr[i]["text"] = L1
-            # -----------------------------------------------------------------------------
+
+            n = lineWork.lower().find('text="')  # Заполнение свойства text, если указано
+            if n != -1:
+                L1 = lineWork[lineWork.lower().find('text="') + 6:]
+                L1 = L1[:L1.find('"')]
+                arr[i]["text"] = L1
 
             # Корректируем размеры и положение объектов в соответствии с константами минимально допустимых размеров
             # ------------------------------------------------------------------------
@@ -192,7 +159,8 @@ def ParserTextFile(fileName, arr):
                 arr[i]["height"] = DEFAULTHEIGHT
 
             # Проверка объекта на конфликт с предыдущими.
-#            Valid = checkValid(arr)
+            Valid = checkValid(arr, logFile)
+            if not Valid: logFile.write("not Valid" + "\n")
 
             # Попытка коррекции объекта перемещением
             # ------------------------------------------------------------------------
@@ -253,7 +221,6 @@ def ParserTextFile(fileName, arr):
             # # ------------------------------------------------------------------------
             logFile.write("OBJECT: " + str(arr[i]) + "\n")  # DEBUG: Отладочный вывод
 
-    logFile.write("\n")  # DEBUG: Отладочный вывод
     logFile.write("End parsing" + "\n")
     logFile.close()
     # if not Errs : DeleteFile(PathName + '.log')
